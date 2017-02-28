56e9b65019bbbb4032281fbb95515c7106e0eabd
adam-f-weathergroup <adam.francisco@weathergroup.com>
2017-02-28 08:56:18 -0500

    added name

--- GitHubBranchingSpike/Program.cs
+++ GitHubBranchingSpike/Program.cs
@@ -10,7 +10,9 @@ namespace GitHubBranchingSpike
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World");
+			Console.WriteLine("What is your name?");
+			var name = Console.ReadLine();
+			Console.WriteLine("Hello " + name);
 			Console.ReadLine();
 		}
 	}
