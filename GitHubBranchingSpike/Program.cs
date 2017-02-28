4dd974490306d7a586dbc9836d16df49a7c26f99
adam-f-weathergroup <adam.francisco@weathergroup.com>
2017-02-28 07:35:45 -0500

    added string formatter extension

--- GitHubBranchingSpike/Program.cs
+++ GitHubBranchingSpike/Program.cs
@@ -10,7 +10,15 @@ namespace GitHubBranchingSpike
 	{
 		static void Main(string[] args)
 		{
+			Console.WriteLine("Enter Your Name");
+			var name = Console.ReadLine();
 			Console.WriteLine("Hello World");
+		}
+	}
+
+	public static class StringExtensions { 
+		public static string Format(this string formatString, params string[] args){
+			return string.Format(formatString, args);
 		}
 	}
 }
